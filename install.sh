05ab406473254c38a3b530d4a29a2e384dca68e9
Nick <33908215+NicholasG04@users.noreply.github.com>
2021-04-12 21:54:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
